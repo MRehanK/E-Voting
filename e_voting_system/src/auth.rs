c1f631c5bb2091dac18b06a1c06415f69315c1cf
CoderunED <94192896+CoderunED@users.noreply.github.com>
2025-10-27 12:40:37 -0400

    Update authentication logic to use Argon2

--- e_voting_system/src/auth.rs
+++ e_voting_system/src/auth.rs
@@ -8,3 +8,26 @@
 // - Differentiate between admin, district, and voter roles
 // - Ensure secure access to system functionality
 // ============================================================
+
+use argon2::{
+    password_hash::{PasswordHash, PasswordHasher, PasswordVerifier, SaltString},
+    Algorithm, Argon2, Params, Version,
+};
+use rand::rngs::OsRng;
+
+pub fn hash_password(pw: &str) -> String {
+    let salt = SaltString::generate(&mut OsRng);
+    // The new version of Argon2 doesn't have Params::recommended(),
+    // so we use Params::new(...) manually.
+    let params = Params::new(15000, 2, 1, None).unwrap(); // memory cost, iterations, parallelism
+    let argon = Argon2::new(Algorithm::Argon2id, Version::V0x13, params);
+    argon.hash_password(pw.as_bytes(), &salt).unwrap().to_string()
+}
+
+pub fn verify_password(hash: &str, pw: &str) -> bool {
+    if let Ok(parsed) = PasswordHash::new(hash) {
+        Argon2::default().verify_password(pw.as_bytes(), &parsed).is_ok()
+    } else {
+        false
+    }
+}
